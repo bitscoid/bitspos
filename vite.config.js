0e0defcfb902571daae6df1a8f9f2a17a0838395
Banten IT Solutions <nurulimamstudio@gmail.com>
2023-10-05 03:16:48 +0700

    Update Vite Broken Image Build

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,12 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue()
+        vue({
+            template: {
+                transformAssetUrls: {
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
 });
